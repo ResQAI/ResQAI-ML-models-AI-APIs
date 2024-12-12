fefd5dd4fc9c7a8002c896e173609c8ba949ee4f
abhaydixit07 <abhaydixit227@gmail.com>
2024-12-13 01:46:51 +0530

    Add Realtime Sourcing to models

--- Google-GenAI-APIs/Landing/main.py
+++ Google-GenAI-APIs/Landing/main.py
@@ -1,42 +1,42 @@
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, jsonify
 import os
-import google.generativeai as genai
-import logging
+import vertexai
+from vertexai.generative_models import (
+    GenerationConfig,
+    GenerativeModel,
+    Tool,
+    grounding,
+)
 from flask_cors import CORS
 from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 app = Flask(__name__)
 CORS(app)
 
-load_dotenv()
-GOOGLE_GENAI_API_KEY=os.getenv('GOOGLE_GENAI_API_KEY')
-genai.configure(api_key=GOOGLE_GENAI_API_KEY)
+GOOGLE_CREDENTIAL_FILE=os.getenv("GOOGLE_CREDENTIAL_FILE")
+PROJECT_ID=os.getenv("PROJECT_ID")
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_CREDENTIAL_FILE
+vertexai.init(project=PROJECT_ID, location="us-central1")
 
-def create_model():
-    generation_config = {
-        "temperature": 0.6,
-        "top_p": 0.9,
-        "top_k": 40,
-        "max_output_tokens": 1500,
-        "response_mime_type": "text/plain",
-    }
 
-    return genai.GenerativeModel(
-        model_name="gemini-1.5-flash-8b",
-        generation_config=generation_config,
-        system_instruction=(
-            "You are ResQAI , disaster management AI. Help users with their queries related to disaster management."
-        ),
-    )
-
-model = create_model()
 
 @app.route('/landing-chat', methods=['POST'])
 def genie():
+    
     try:
-        
+        model = GenerativeModel(
+            model_name="gemini-1.5-flash-001",
+            system_instruction=[
+                "You are ResQAI, an AI expert in disaster management. Help users with grounded responses to their queries about disasters."
+            ],
+        )
 
+
+        tool = Tool.from_google_search_retrieval(grounding.GoogleSearchRetrieval())
         user_query = request.json.get('query')
         chat_history = request.json.get('chat_history', [])
         if not user_query:
@@ -45,18 +45,27 @@ def genie():
         if not isinstance(chat_history, list):
             return jsonify({'error': 'chat_history must be a list of JSON objects.'}), 400
 
-        
-        chat_session = model.start_chat(history=chat_history)      
+
+        prompt = chat_history
+        prompt.append({"role": "user", "parts": [{"text": user_query}]})
 
         
-        response = chat_session.send_message(user_query)
+        response = model.generate_content(
+            contents=prompt,
+            tools=[tool],
+            generation_config=GenerationConfig(
+                temperature=0.0
+            ),
+        )
+        
+        
+        model_reply = response.text
 
         
-
-        return jsonify({'response': response.text}), 200
+        return jsonify({'response': model_reply}), 200
 
     except Exception as e:
-        logging.error(f"Error processing query: {str(e)}")
+        
         return jsonify({'error': 'An error occurred while processing the request.', 'details': str(e)}), 500
 
 if __name__ == '__main__':
